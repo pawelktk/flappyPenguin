9371bda0ab0671448253246d541a667107bd9fcf
Paweł <67957305+pawelktk@users.noreply.github.com>
2025-01-29 15:07:10 +0100

    formatting

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,10 @@ struct Pipe {
 
 impl Bird {
     fn new() -> Self {
-        Self { y: screen_height() / 2.0, velocity: 0.0 }
+        Self {
+            y: screen_height() / 2.0,
+            velocity: 0.0,
+        }
     }
     fn update(&mut self) {
         self.velocity += GRAVITY;
@@ -40,14 +43,24 @@ impl Bird {
 
 impl Pipe {
     fn new(x: f32, height: f32) -> Self {
-        Self { x, height, passed: false }
+        Self {
+            x,
+            height,
+            passed: false,
+        }
     }
     fn update(&mut self) {
         self.x -= PIPE_SPEED;
     }
     fn draw(&self) {
         draw_rectangle(self.x, 0.0, PIPE_WIDTH, self.height, GREEN);
-        draw_rectangle(self.x, self.height + PIPE_GAP, PIPE_WIDTH, screen_height(), GREEN);
+        draw_rectangle(
+            self.x,
+            self.height + PIPE_GAP,
+            PIPE_WIDTH,
+            screen_height(),
+            GREEN,
+        );
     }
     fn collides_with(&self, bird: &Bird) -> bool {
         let bird_x = screen_width() / 4.0;
@@ -73,61 +86,76 @@ async fn main() {
         match state {
             GameState::MainMenu => {
                 clear_background(SKYBLUE);
-                draw_text("Flappy Bird", screen_width() / 2.0 - 80.0, screen_height() / 2.0 - 40.0, 40.0, WHITE);
-                draw_text("Press SPACE to Start", screen_width() / 2.0 - 120.0, screen_height() / 2.0, 30.0, WHITE);
+                draw_text(
+                    "Flappy Bird",
+                    screen_width() / 2.0 - 80.0,
+                    screen_height() / 2.0 - 40.0,
+                    40.0,
+                    WHITE,
+                );
+                draw_text(
+                    "Press SPACE to Start",
+                    screen_width() / 2.0 - 120.0,
+                    screen_height() / 2.0,
+                    30.0,
+                    WHITE,
+                );
                 if is_key_pressed(KeyCode::Space) {
                     state = GameState::Playing;
                 }
-                        next_frame().await;
+                next_frame().await;
             }
             GameState::Playing => {
-				clear_background(SKYBLUE);
-				bird.update();
-				bird.draw();
+                clear_background(SKYBLUE);
+                bird.update();
+                bird.draw();
 
-				if let Some(last_pipe) = pipes.last() {
-				    if last_pipe.x < screen_width() - 200.0 {
-				        pipes.push(Pipe::new(screen_width(), rand::gen_range(50.0, 300.0)));
-				    }
-				}
+                if let Some(last_pipe) = pipes.last() {
+                    if last_pipe.x < screen_width() - 200.0 {
+                        pipes.push(Pipe::new(screen_width(), rand::gen_range(50.0, 300.0)));
+                    }
+                }
 
-				// Collect indices of pipes that should be removed
-				let mut reset_game = false;
-				let mut new_pipes = Vec::new();
+                // Collect indices of pipes that should be removed
+                let mut reset_game = false;
+                let mut new_pipes = Vec::new();
 
-				for pipe in &mut pipes {
-				    pipe.update();
-				    pipe.draw();
+                for pipe in &mut pipes {
+                    pipe.update();
+                    pipe.draw();
 
-				    if pipe.collides_with(&bird) || bird.y > screen_height() {
-				        reset_game = true;
-				    } else if pipe.x + PIPE_WIDTH > 0.0 {
-				        new_pipes.push(Pipe::new(pipe.x, pipe.height));
-				    }
-				    
-				}
-				
-				score += 1;
+                    if pipe.collides_with(&bird) || bird.y > screen_height() {
+                        reset_game = true;
+                    } else if pipe.x + PIPE_WIDTH > 0.0 {
+                        new_pipes.push(Pipe::new(pipe.x, pipe.height));
+                    }
+                }
 
+                score += 1;
 
-				if reset_game {
-				    bird = Bird::new();
-				    pipes = vec![Pipe::new(screen_width(), rand::gen_range(50.0, 300.0))];
-				    if score > high_score {
-				    	high_score = score;
-				    }
-				    score = 0;
-				} else {
-				    pipes = new_pipes;
-				}
-				
-				draw_text(&format!("Score: {}", score), 20.0, 40.0, 30.0, WHITE);
-				draw_text(&format!("High score: {}", high_score), 20.0, 80.0, 30.0, WHITE);
+                if reset_game {
+                    bird = Bird::new();
+                    pipes = vec![Pipe::new(screen_width(), rand::gen_range(50.0, 300.0))];
+                    if score > high_score {
+                        high_score = score;
+                    }
+                    score = 0;
+                } else {
+                    pipes = new_pipes;
+                }
 
+                draw_text(&format!("Score: {}", score), 20.0, 40.0, 30.0, WHITE);
+                draw_text(
+                    &format!("High score: {}", high_score),
+                    20.0,
+                    80.0,
+                    30.0,
+                    WHITE,
+                );
 
-				next_frame().await;
+                next_frame().await;
             }
         }
-
     }
 }
+
